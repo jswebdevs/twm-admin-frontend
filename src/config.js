0066583632c5b778d7eab217f2078d6b1a0389b6
jswebdevs <jskeya116@gmail.com>
2026-02-08 22:03:37 +0600

    Uploading Frontend

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 // Fallback to localhost:5000 if env var is missing
-// export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
-export const API_BASE_URL = "http://localhost:5000";
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
+// export const API_BASE_URL = "http://localhost:5000";
 export const CLOUDINARY_CLOUD_NAME = import.meta.env.VITE_CLOUDINARY_CLOUD_NAME;
 export const CLOUDINARY_UPLOAD_PRESET = import.meta.env.VITE_CLOUDINARY_UPLOAD_PRESET;
 
